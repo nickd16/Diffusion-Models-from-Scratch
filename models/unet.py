f61d8ba3a545174a2ee5e72a59e48019eca614da
nickd16 <nickd16718@gmail.com>
2024-06-30 01:59:01 -0400

    finished unet for ddpm

--- models/unet.py
+++ models/unet.py
@@ -2,26 +2,30 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange
+from typing import List
+import random
+import math
+import pdb
 
 class ResBlock(nn.Module):
-    def __init__(self, C, num_groups, dropout_prob):
+    def __init__(self, C: int, num_groups: int, dropout_prob: float):
         super().__init__()
         self.relu = nn.ReLU(inplace=True)
         self.gnorm1 = nn.GroupNorm(num_groups=num_groups, num_channels=C)
         self.gnorm2 = nn.GroupNorm(num_groups=num_groups, num_channels=C)
-        self.conv1 = nn.Conv2d(C, C, padding=1)
-        self.conv2 = nn.Conv2d(C, C, padding=1)
+        self.conv1 = nn.Conv2d(C, C, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(C, C, kernel_size=3, padding=1)
         self.dropout = nn.Dropout(p=dropout_prob, inplace=True)
 
-    def forward(self, x):
+    def forward(self, x, embeddings):
+        x = x + embeddings[:, :x.shape[1], :, :]
         r = self.conv1(self.relu(self.gnorm1(x)))
-        if self.drop:
-            r = self.dropout(r)
+        r = self.dropout(r)
         r = self.conv2(self.relu(self.gnorm2(r)))
         return r + x
     
 class Attention(nn.Module):
-    def __init__(self, C, num_heads, dropout_prob):
+    def __init__(self, C: int, num_heads:int , dropout_prob: float):
         super().__init__()
         self.proj1 = nn.Linear(C, C*3)
         self.proj2 = nn.Linear(C, C)
@@ -39,10 +43,99 @@ class Attention(nn.Module):
         x = self.proj2(x)
         return rearrange(x, 'b h w C -> b C h w')
 
+class UnetLayer(nn.Module):
+    def __init__(self, 
+            upscale: bool, 
+            attention: bool, 
+            num_groups: int, 
+            dropout_prob: float,
+            num_heads: int,
+            C: int):
+        super().__init__()
+        self.ResBlock1 = ResBlock(C=C, num_groups=num_groups, dropout_prob=dropout_prob)
+        self.ResBlock2 = ResBlock(C=C, num_groups=num_groups, dropout_prob=dropout_prob)
+        if upscale:
+            self.conv = nn.ConvTranspose2d(C, C//2, kernel_size=4, stride=2, padding=1)
+        else:
+            self.conv = nn.Conv2d(C, C*2, kernel_size=3, stride=2, padding=1)
+        if attention:
+            self.attention_layer = Attention(C, num_heads=num_heads, dropout_prob=dropout_prob)
+
+    def forward(self, x, embeddings):
+        x = self.ResBlock1(x, embeddings)
+        if hasattr(self, 'attention_layer'):
+            x = self.attention_layer(x)
+        x = self.ResBlock2(x, embeddings)
+        return self.conv(x), x
+
+class SinusoidalEmbeddings(nn.Module):
+    def __init__(self, time_steps:int, embed_dim: int):
+        super().__init__()
+        position = torch.arange(time_steps).unsqueeze(1).float()
+        div = torch.exp(torch.arange(0, embed_dim, 2).float() * -(math.log(10000.0) / embed_dim))
+        embeddings = torch.zeros(time_steps, embed_dim, requires_grad=False)
+        embeddings[:, 0::2] = torch.sin(position * div)
+        embeddings[:, 1::2] = torch.cos(position * div)
+        self.embeddings = embeddings
+
+    def forward(self, x, t):
+        embeds = self.embeddings[t].to(x.device)
+        return embeds[:, :, None, None]
+
+class UNET(nn.Module):
+    def __init__(self,
+            Channels: List = [64, 128, 256, 512, 512, 384],
+            Attentions: List = [False, True, False, False, False, True],
+            Upscales: List = [False, False, False, True, True, True],
+            num_groups: int = 32,
+            dropout_prob: float = 0.0,
+            num_heads: int = 8,
+            input_channels: int = 1,
+            output_channels: int = 1,
+            time_steps: int = 1000):
+        super().__init__()
+        self.num_layers = len(Channels)
+        self.shallow_conv = nn.Conv2d(input_channels, Channels[0], kernel_size=3, padding=1)
+        out_channels = (Channels[-1]//2)+Channels[0]
+        self.late_conv = nn.Conv2d(out_channels, out_channels//2, kernel_size=3, padding=1)
+        self.output_conv = nn.Conv2d(out_channels//2, output_channels, kernel_size=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.embeddings = SinusoidalEmbeddings(time_steps=time_steps, embed_dim=max(Channels))
+        for i in range(self.num_layers):
+            layer = UnetLayer(
+                upscale=Upscales[i],
+                attention=Attentions[i],
+                num_groups=num_groups,
+                dropout_prob=dropout_prob,
+                C=Channels[i],
+                num_heads=num_heads
+            )
+            setattr(self, f'Layer{i+1}', layer)
+
+    def forward(self, x, t):
+        x = self.shallow_conv(x)
+        residuals = []
+        for i in range(self.num_layers//2):
+            layer = getattr(self, f'Layer{i+1}')
+            embeddings = self.embeddings(x, t)
+            x, r = layer(x, embeddings)
+            residuals.append(r)
+        for i in range(self.num_layers//2, self.num_layers):
+            layer = getattr(self, f'Layer{i+1}')
+            x = torch.concat((layer(x, embeddings)[0], residuals[self.num_layers-i-1]), dim=1)
+        return self.output_conv(self.relu(self.late_conv(x)))
+
 def main():
-    att = Attention(512, 8, 0).cuda()
-    x = torch.randn(16, 512, 16, 16).cuda()
-    print(att(x).shape)
+    x = torch.randn(16, 1, 32, 32).cuda()
+    t = [random.randint(0, 999) for _ in range(16)]
+    model = UNET().cuda()
+    print(sum(p.numel() for p in model.parameters() if p.requires_grad))
+
+    # a = torch.randn((16, 32, 4, 4))
+    # b = torch.randn((16, 32, 4, 4))
+    # print(torch.concat((a,b), dim=1).shape)
+
+
 
 if __name__ == '__main__':
     main()
