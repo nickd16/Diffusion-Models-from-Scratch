3f8b595acb2440e8d3cdd0f33f5dc904b1631231
nickd16 <nickd16718@gmail.com>
2024-07-01 19:14:08 -0400

    added ddpm scheduler and completed ddpm inference

---
+++ models/utils.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+from typing import List
+import random
+import math
+import pdb
+
+class DDPM_Scheduler(nn.Module):
+    def __init__(self, num_time_steps: int=1000):
+        super().__init__()
+        self.beta = torch.linspace(1e-4, 0.02, num_time_steps, requires_grad=False)
+        alpha = 1 - self.beta
+        self.alpha = torch.cumprod(alpha, dim=0).requires_grad_(False)
+
+    def forward(self, t):
+        return self.beta[t], self.alpha[t]
+
+def main():
+    scheduler = DDPM_Scheduler(num_time_steps=1000)
+    print(scheduler(999))
+
+if __name__ == '__main__':
+    main()
